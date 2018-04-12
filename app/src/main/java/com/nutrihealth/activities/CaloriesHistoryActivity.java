8e8969f28ca14a6aaf5aab2d0c426e2827b909ba
RazAndroid <razvan.cirstei@adonissoft.com>
2018-04-12 18:05:17 +0300

    History start

---
+++ app/src/main/java/com/nutrihealth/activities/CaloriesHistoryActivity.java
@@ -0,0 +1,16 @@
+package com.nutrihealth.activities;
+
+import com.nutrihealth.base.BaseActivity;
+
+/**
+ * Created by Teodora on 12.04.2018.
+ */
+
+public class CaloriesHistoryActivity extends BaseActivity {
+
+    public enum Section {
+        TODAY,
+        HISTORY
+    }
+
+}
