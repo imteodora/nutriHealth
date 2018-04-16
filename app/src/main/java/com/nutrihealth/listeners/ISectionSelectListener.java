53c9a487c9d10771c75fb10adb305661718d81d2
RazAndroid <razvan.cirstei@adonissoft.com>
2018-04-16 18:04:41 +0300

    ...

---
+++ app/src/main/java/com/nutrihealth/listeners/ISectionSelectListener.java
@@ -0,0 +1,11 @@
+package com.nutrihealth.listeners;
+
+import com.nutrihealth.activities.CaloriesHistoryActivity;
+
+/**
+ * Created by erkut on 10/24/17.
+ */
+
+public interface ISectionSelectListener {
+    void onSectionSelected(final CaloriesHistoryActivity.Section section);
+}
